78e6106de4dd92e3dc2c8b7f228bb3931d7b8eb1
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-08-14 17:50:30 +0530

    fix: point system-config to RC5 for gh-pages

--- system-config-io.js
+++ system-config-io.js
@@ -5,8 +5,8 @@
  */
 (function(global) {
 
-  var ngVer = '@2.0.0-rc.4'; // lock in the angular package version; do not let it float to current!
-  var routerVer = '@3.0.0-beta.2'; // lock router version
+  var ngVer = '@2.0.0-rc.5'; // lock in the angular package version; do not let it float to current!
+  var routerVer = '@3.0.0-rc.1'; // lock router version
   var formsVer = '@0.2.0'; // lock forms version
 
   //map tells the System loader where to look for things
