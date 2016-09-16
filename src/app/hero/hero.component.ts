df63ce0572cffd58a0ca61103cadfc6cdeddaf87
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-09-17 03:01:37 +0530

    feat: upgrade to 2.0.0 final !!!!

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
 import { HeroDetail } from './hero-detail';
 import { HeroListComponent } from './hero-list';
 
--- src/app/hero/hero.routes.ts
+++ src/app/hero/hero.routes.ts
@@ -1,15 +1,15 @@
-import { RouterConfig } from '@angular/router';
+import { Route } from '@angular/router';
 import { HeroComponent } from './hero.component';
 
 import { HeroDetail } from './hero-detail';
 import { HeroListComponent } from './hero-list';
 
-const CHILD_ROUTES: RouterConfig = [
+const CHILD_ROUTES: Route[] = [
   { path: '', pathMatch: 'full', component: HeroListComponent },
   { path: 'detail/:id', component: HeroDetail }
 ];
 
-export const MODULE_ROUTES: RouterConfig = [
+export const MODULE_ROUTES: Route[] = [
   { path: 'heroes', component: HeroComponent, children: [ ...CHILD_ROUTES ] }
 ]
 
